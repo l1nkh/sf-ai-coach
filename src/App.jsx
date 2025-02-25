1c42d775bc8db2263d480f7b17692313039b070d
l1nkh <diogo.m.henriques98@gmail.com>
2025-02-25 00:29:03 +0000

    Develop Header, Button and Section Components
    Also so added tons of assets

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Button from './components/Button';
+import Header from './components/Header';
+import Section from './components/Section';
+
+const App = () => {
+  return (
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  )
+}
+
+export default App;
